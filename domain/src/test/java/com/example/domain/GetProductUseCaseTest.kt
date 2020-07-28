0a58c4e9757dd5b472125022939b75845cd71597
Lasota Oskar <o.lasota@reply.com>
2020-07-28 14:39:46 +0100

    chore: test coroutines

--- domain/src/test/java/com/example/domain/GetProductUseCaseTest.kt
+++ domain/src/test/java/com/example/domain/GetProductUseCaseTest.kt
@@ -1,8 +1,21 @@
 package com.example.domain
 
+import com.example.data.repo.home.ProductRepository
+import com.example.domain.home.GetProductUseCaseImpl
+import com.example.model.ProductDTO
+import com.flextrade.jfixture.FixtureAnnotations
+import com.flextrade.jfixture.annotations.Fixture
+import com.nhaarman.mockito_kotlin.atLeastOnce
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.Single
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,6 +25,41 @@ import org.junit.Assert.*
 class GetProductUseCaseTest {
 
 
+    @Mock
+    lateinit var repo : ProductRepository
+
+    lateinit var sut : GetProductUseCaseImpl
+
+    @Fixture
+    lateinit var fixtItemsDTO: List<ProductDTO>
+
+
+    @Before
+    fun setup(){
+        MockitoAnnotations.initMocks(this)
+        FixtureAnnotations.initFixtures(this)
+        sut = GetProductUseCaseImpl(repo)
+    }
+
+    @Test
+    fun `test getProducts()`(){
+        whenever(repo.getProducts()).thenReturn(Single.just(fixtItemsDTO))
+
+        sut.getProducts()
+
+        verify(repo, atLeastOnce()).getProducts()
+    }
+
+    @Test
+    fun `coroutine test getCoroutineProducts()`(){
+        runBlocking {
+            whenever(repo.getCoroutinesProducts()).thenReturn(fixtItemsDTO)
+            val result = sut.getCoroutineProducts()
+            verify(repo, atLeastOnce()).getCoroutinesProducts()
+            assertTrue(fixtItemsDTO == result)
+        }
+
+    }
 
 
 
